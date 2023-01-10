3703348def2d73c234015367b6792909632d1fa0
gitlab-runner <do-not-reply@cypress.com>
2023-01-10 02:19:42 -0600

    Upload mtb-example-xmc-ccu8-pwm-complementary [407]

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ One CCU8 slice is configured to generate complementary PWM signals in the projec
 
 ## Requirements
 
-- [ModusToolbox&trade; software](https://www.cypress.com/products/modustoolbox-software-environment) v3.0
+- [ModusToolbox&trade; software](https://www.infineon.com/products/modustoolbox-software-environment) v3.0
 - [SEGGER J-Link software](https://www.segger.com/downloads/jlink/#J-LinkSoftwareAndDocumentationPack)
 - Programming language: C
 - Associated parts: All [XMC4000 MCU](https://www.infineon.com/cms/en/product/microcontroller/32-bit-industrial-microcontroller-based-on-arm-cortex-m/32-bit-xmc4000-industrial-microcontroller-arm-cortex-m4/) parts
@@ -19,8 +19,18 @@ One CCU8 slice is configured to generate complementary PWM signals in the projec
 
 ## Supported kits (make variable 'TARGET')
 
-- [XMC1400 boot kit](https://www.infineon.com/cms/en/product/evaluation-boards/kit_xmc14_boot_001/) (`KIT_XMC14_BOOT_001`) - Default value of `TARGET`
+- [XMC1300 boot kit](https://www.infineon.com/cms/en/product/evaluation-boards/kit_xmc13_boot_001/) (`KIT_XMC13_BOOT_001`)
+- [XMC1400 boot kit](https://www.infineon.com/cms/en/product/evaluation-boards/kit_xmc14_boot_001/) (`KIT_XMC14_BOOT_001`) - Default
+ value of `TARGET`
+- [XMC4200 Platform2Go kit](https://www.infineon.com/cms/en/product/evaluation-boards/kit_xmc_plt2go_xmc4200/) (`KIT_XMC_PLT2GO_XMC4
+200`)
+- [XMC4300 relax EtherCAT kit](https://www.infineon.com/cms/en/product/evaluation-boards/kit_xmc43_relax_ecat_v1/) (`KIT_XMC43_RELAX
+_ECAT_V1`)
+- [XMC4400 Platform2Go kit](https://www.infineon.com/cms/en/product/evaluation-boards/kit_xmc_plt2go_xmc4400/) (`KIT_XMC_PLT2GO_XMC4
+400`)
+- [XMC4500 relax kit](https://www.infineon.com/cms/en/product/evaluation-boards/kit_xmc45_relax_v1/) (`KIT_XMC45_RELAX_V1`)
 - [XMC4700 relax kit](https://www.infineon.com/cms/en/product/evaluation-boards/kit_xmc47_relax_v1/) (`KIT_XMC47_RELAX_V1`)
+- [XMC4800 relax EtherCAT kit](https://www.infineon.com/cms/en/product/evaluation-boards/kit_xmc48_relax_ecat_v1/) (`KIT_XMC48_RELAX_ECAT_V1`)
 
 ## Hardware setup
 
@@ -36,11 +46,11 @@ Create the project and open it using one of the following:
 
 <details open><summary><b>In Eclipse IDE for ModusToolbox&trade; software</b></summary>
 
-1. Click the **New Application** link in the **Quick Panel** (or, use **File** > **New** > **ModusToolbox Application**). This launches the [Project Creator](https://www.cypress.com/ModusToolboxProjectCreator) tool.
+1. Click the **New Application** link in the **Quick Panel** (or, use **File** > **New** > **ModusToolbox Application**). This launches the [Project Creator](https://www.infineon.com/ModusToolboxProjectCreator) tool.
 
 2. Pick a kit supported by the code example from the list shown in the **Project Creator - Choose Board Support Package (BSP)** dialog.
 
-   When you select a supported kit, the example is reconfigured automatically to work with the kit. To work with a different supported kit later, use the [Library Manager](https://www.cypress.com/ModusToolboxLibraryManager) to choose the BSP for the supported kit. You can use the Library Manager to select or update the BSP and firmware libraries used in this application. To access the Library Manager, click the link from the **Quick Panel**.
+   When you select a supported kit, the example is reconfigured automatically to work with the kit. To work with a different supported kit later, use the [Library Manager](https://www.infineon.com/ModusToolboxLibraryManager) to choose the BSP for the supported kit. You can use the Library Manager to select or update the BSP and firmware libraries used in this application. To access the Library Manager, click the link from the **Quick Panel**.
 
    You can also just start the application creation process again and select a different kit.
 
@@ -54,7 +64,7 @@ Create the project and open it using one of the following:
 
 6. Click **Create** to complete the application creation process.
 
-For more details, see the [Eclipse IDE for ModusToolbox&trade; software user guide](https://www.cypress.com/MTBEclipseIDEUserGuide) (locally available at *{ModusToolbox&trade; software install directory}/ide_{version}/docs/mt_ide_user_guide.pdf*).
+For more details, see the [Eclipse IDE for ModusToolbox&trade; software user guide](https://www.infineon.com/MTBEclipseIDEUserGuide) (locally available at *{ModusToolbox&trade; software install directory}/ide_{version}/docs/mt_ide_user_guide.pdf*).
 
 </details>
 
@@ -81,7 +91,7 @@ The following example will clone the "[CCU8 PWM Complementary](https://github.co
    project-creator-cli --board-id KIT_XMC47_RELAX_V1 --app-id mtb-example-xmc-ccu8-pwm-complementary --user-app-name FceCrc --target-dir "C:/mtb_projects"
    ```
 
-**Note:** The project-creator-cli tool uses the `git clone` and `make getlibs` commands to fetch the repository and import the required libraries. For details, see the "Project creator tools" section of the [ModusToolbox&trade; software user guide](https://www.cypress.com/ModusToolboxUserGuide) (locally available at *{ModusToolbox&trade; software install directory}/docs_{version}/mtb_user_guide.pdf*).
+**Note:** The project-creator-cli tool uses the `git clone` and `make getlibs` commands to fetch the repository and import the required libraries. For details, see the "Project creator tools" section of the [ModusToolbox&trade; software user guide](https://www.infineon.com/ModusToolboxUserGuide) (locally available at *{ModusToolbox&trade; software install directory}/docs_{version}/mtb_user_guide.pdf*).
 
 </details>
 
@@ -93,7 +103,7 @@ The following example will clone the "[CCU8 PWM Complementary](https://github.co
 
 2. Export the application to a supported IDE using the `make <ide>` command.
 
-   For a list of supported IDEs and more details, see the "Exporting to IDEs" section of the [ModusToolbox&trade; software user guide](https://www.cypress.com/ModusToolboxUserGuide) (locally available at *{ModusToolbox&trade; software install directory}/docs_{version}/mtb_user_guide.pdf*).
+   For a list of supported IDEs and more details, see the "Exporting to IDEs" section of the [ModusToolbox&trade; software user guide](https://www.infineon.com/ModusToolboxUserGuide) (locally available at *{ModusToolbox&trade; software install directory}/docs_{version}/mtb_user_guide.pdf*).
 
 3. Follow the instructions displayed in the terminal to create or import the application as an IDE project.
 
@@ -112,7 +122,8 @@ The following example will clone the "[CCU8 PWM Complementary](https://github.co
 3. Observe the CCU8 slice direct and inverted outputs on an oscilloscope. See the dead-time inserted between transitions (Figure 1).
 
    Output pins:
-   - XMC4000 kits  P0.3 (DIR),  P0.0 (INV)
+   - XMC4300 EtherCAT kit P2.9 (DIR),  P1.5 (INV)
+   - All other XMC4000 kits P0.3 (DIR),  P0.0 (INV)
    - XMC1000 Kits  P0.0 (DIR),  P0.1 (INV)
 
    **Figure 1. Complementary PWM signals generated with dead-time**
@@ -122,7 +133,7 @@ The following example will clone the "[CCU8 PWM Complementary](https://github.co
 
 ## Debugging
 
-You can debug the example to step through the code. In the IDE, use the **\<Application Name> Debug (JLink)** configuration in the **Quick Panel**. For more details, see the "Program and debug" section in the [Eclipse IDE for ModusToolbox&trade; user guide](https://www.cypress.com/MTBEclipseIDEUserGuide).
+You can debug the example to step through the code. In the IDE, use the **\<Application Name> Debug (JLink)** configuration in the **Quick Panel**. For more details, see the "Program and debug" section in the [Eclipse IDE for ModusToolbox&trade; user guide](https://www.infineon.com/MTBEclipseIDEUserGuide).
 
 ## Design and implementation
 
@@ -142,7 +153,7 @@ Code examples  | [Using ModusToolbox&trade; software](https://github.com/Infineo
 Device documentation | [XMC4000 family datasheets](https://www.infineon.com/cms/en/product/microcontroller/32-bit-industrial-microcontroller-based-on-arm-cortex-m/32-bit-xmc4000-industrial-microcontroller-arm-cortex-m4/#document-group-myInfineon-49) <br> [XMC4000 family technical reference manuals](https://www.infineon.com/cms/en/product/microcontroller/32-bit-industrial-microcontroller-based-on-arm-cortex-m/32-bit-xmc4000-industrial-microcontroller-arm-cortex-m4/#document-group-myInfineon-44) <br> [XMC1000 family datasheets](https://www.infineon.com/cms/en/product/microcontroller/32-bit-industrial-microcontroller-based-on-arm-cortex-m/32-bit-xmc1000-industrial-microcontroller-arm-cortex-m0/#document-group-myInfineon-49) <br> [XMC1000 family technical reference manuals](https://www.infineon.com/cms/en/product/microcontroller/32-bit-industrial-microcontroller-based-on-arm-cortex-m/32-bit-xmc1000-industrial-microcontroller-arm-cortex-m0/#document-group-myInfineon-44)
 Development kits | [XMC&trade; eval boards](https://www.infineon.com/cms/en/product/microcontroller/32-bit-industrial-microcontroller-based-on-arm-cortex-m/#boards)
 Libraries on GitHub  | [mtb-xmclib-cat3](https://github.com/Infineon/mtb-xmclib-cat3) – XMC&trade; peripheral driver library (XMCLib)
-Tools  | [Eclipse IDE for ModusToolbox&trade; software](https://www.cypress.com/modustoolbox) – ModusToolbox&trade; software is a collection of easy-to-use software and tools enabling rapid development with Infineon MCUs, covering applications from embedded sense and control to wireless and cloud-connected systems using AIROC&trade; Wi-Fi and Bluetooth® connectivity devices.
+Tools  | [Eclipse IDE for ModusToolbox&trade; software](https://www.infineon.com/modustoolbox) – ModusToolbox&trade; software is a collection of easy-to-use software and tools enabling rapid development with Infineon MCUs, covering applications from embedded sense and control to wireless and cloud-connected systems using AIROC&trade; Wi-Fi and Bluetooth® connectivity devices.
 
 ## Other resources
 
@@ -160,6 +171,7 @@ Document title: *CE232581* – *XMC&trade; MCU: CCU8 PWM complementary*
 | 1.0.1   | Updated README                      |
 | 1.1.0   | Added support for new kits          |
 | 2.0.0   | Updated to support ModusToolbox&trade; software v3.0; CE will not be backwards compatible with previous versions of ModusToolbox™ software |
+| 2.1.0   | Added support for CCU8 personality and new Kits |
 ------
 
 All other trademarks or registered trademarks referenced herein are the property of their respective owners.
--- main.c
+++ main.c
@@ -43,14 +43,11 @@
 #include "cybsp.h"
 #include "cy_utils.h"
 #include "cy_retarget_io.h"
-#include "xmc_common.h"
-#include "xmc_ccu8.h"
-#include "xmc_gpio.h"
-
 
 /*******************************************************************************
 * Defines
 *******************************************************************************/
+
 /* Define macro to enable/disable printing of debug messages */
 #define ENABLE_XMC_DEBUG_PRINT              (0)
 
@@ -58,134 +55,6 @@
 #if ENABLE_XMC_DEBUG_PRINT
 #define DEBUG_LOOP_COUNT_MAX                (1U)
 #endif
-
-#define    PWM_PERIOD                     720U
-#define    PWM_COMPARE                    360U
-
-#if (UC_SERIES == XMC14)
-
-#define CCU8_MODULE_PTR                   CCU80
-#define PWM_SLICE_PTR                     CCU80_CC80
-#define PWM_SLICE_NUMBER                  (0U)
-#define PWM_OUTPUT_GPIO_PORT              (XMC_GPIO_PORT_t *) PORT0_BASE
-#define PWM_OUTPUT_GPIO_PIN               0U
-#define PWM_INV_OUTPUT_GPIO_PORT          (XMC_GPIO_PORT_t *) PORT0_BASE
-#define PWM_INV_OUTPUT_GPIO_PIN           1U
-#define PWM_SHADOW_TRANSFER_MASK          XMC_CCU8_SHADOW_TRANSFER_SLICE_0
-#define COMPARE_CHANNEL                   XMC_CCU8_SLICE_COMPARE_CHANNEL_1
-#define SLICE_STATUS_CHANNEL              XMC_CCU8_SLICE_STATUS_CHANNEL_1
-
-#endif
-
-#if (UC_SERIES == XMC47)
-#define CCU8_MODULE_PTR                   CCU80
-#define PWM_SLICE_PTR                     CCU80_CC82
-#define PWM_SLICE_NUMBER                  (2U)
-#define PWM_OUTPUT_GPIO_PORT              (XMC_GPIO_PORT_t *) PORT0_BASE
-#define PWM_OUTPUT_GPIO_PIN               3U
-#define PWM_INV_OUTPUT_GPIO_PORT          (XMC_GPIO_PORT_t *) PORT0_BASE
-#define PWM_INV_OUTPUT_GPIO_PIN           0U
-#define PWM_SHADOW_TRANSFER_MASK          XMC_CCU8_SHADOW_TRANSFER_SLICE_2
-#define COMPARE_CHANNEL                   XMC_CCU8_SLICE_COMPARE_CHANNEL_1
-#define SLICE_STATUS_CHANNEL              XMC_CCU8_SLICE_STATUS_CHANNEL_1
-#endif
-
-/*******************************************************************************
-* Global Data
-*******************************************************************************/
-/*
- * CCU8 slice configuration structure to generate PWM output
- */
-XMC_CCU8_SLICE_COMPARE_CONFIG_t ccu80_slice_pwm_config =
-{
-        .timer_mode            = (uint32_t)XMC_CCU8_SLICE_TIMER_COUNT_MODE_EA,          /* Edge Aligned Mode */
-        .monoshot              = (uint32_t)XMC_CCU8_SLICE_TIMER_REPEAT_MODE_REPEAT,     /* Continous Mode */
-        .shadow_xfer_clear     = 0U,                                                    /* Shadow tranfer disabled on timer clear */
-        .dither_timer_period   = 0U,                                                    /* Dithering not enabled*/
-        .dither_duty_cycle     = 0U,
-
-        .prescaler_mode        = (uint32_t)XMC_CCU8_SLICE_PRESCALER_MODE_NORMAL,        /* Normal prescaler mode */
-
-        .mcm_ch1_enable        = 0U,                                                    /* Multi Channel Mode Disabled  for Ch1*/
-        .mcm_ch2_enable        = 0U,                                                    /* Multi Channel Mode Disabled for Ch2*/
-
-        .slice_status          = (uint32_t)SLICE_STATUS_CHANNEL,                        /* SLICE_STATUS_CHANNEL drives slice status output */
-
-        .passive_level_out0    = (uint32_t)XMC_CCU8_SLICE_OUTPUT_PASSIVE_LEVEL_LOW,     /* Out0 passive level set low */
-        .passive_level_out1    = (uint32_t)XMC_CCU8_SLICE_OUTPUT_PASSIVE_LEVEL_LOW,     /* Out1 passive level set low */
-        .passive_level_out2    = (uint32_t)XMC_CCU8_SLICE_OUTPUT_PASSIVE_LEVEL_LOW,     /* Out2 passive level set low */
-        .passive_level_out3    = (uint32_t)XMC_CCU8_SLICE_OUTPUT_PASSIVE_LEVEL_LOW,     /* Out3 passive level set low */
-
-        .asymmetric_pwm        = 0U,                                                    /* Symmetric PWM */
-#if !defined(CCU8V3)
-        .invert_out0           = 0U,                                                    /* Direct connection to Out0 */
-        .invert_out1           = 1U,                                                    /* Inverted connection to Out1 */
-        .invert_out2           = 0U,                                                    /* Direct connection to Out2 */
-        .invert_out3           = 1U,                                                    /* Inverted connection to Out3 */
-#else
-        .selector_out0         = XMC_CCU8_SOURCE_OUT0_ST1,                              /* Direct connection to Out0 */
-        .selector_out1         = XMC_CCU8_SOURCE_OUT1_INV_ST1,                          /* Inverted connection to Out1 */
-        .selector_out2         = XMC_CCU8_SOURCE_OUT2_ST2,                              /* Direct connection to Out2 */
-        .selector_out3         = XMC_CCU8_SOURCE_OUT3_INV_ST2,                          /* Inverted connection to Out3 */
-#endif
-        .prescaler_initval     = 1U,                                                    /* Prescaler divider  value */
-        .float_limit           = 0U,                                                    /*  */
-        .dither_limit          = 0U,                                                    /* Dithering Range */
-        .timer_concatenation   = 0U,                                                    /* Disable concatenation of timer */
-};
-
-/*
- * GPIO configuration for PWM outputs (Direct and Invert outputs)
- */
-#if (UC_SERIES == XMC14)
-
-const XMC_GPIO_CONFIG_t  ccu8_pwm_gpio1_config    =
-{
-   .mode                = XMC_GPIO_MODE_OUTPUT_PUSH_PULL_ALT5,                          /* Alternate Mode 5 to connect to CCU8 output */
-   .output_level        = XMC_GPIO_OUTPUT_LEVEL_LOW,                                    /* Output level of pin */
-   .input_hysteresis    = XMC_GPIO_INPUT_HYSTERESIS_STANDARD                            /* Pad hysteresis of pin */
-};
-const XMC_GPIO_CONFIG_t  ccu8_pwm_gpio2_config    =
-{
-   .mode                = XMC_GPIO_MODE_OUTPUT_PUSH_PULL_ALT5,                          /* Alternate Mode 5 to connect to CCU8 output */
-   .output_level        = XMC_GPIO_OUTPUT_LEVEL_LOW,                                    /* Output level of pin */
-   .input_hysteresis    = XMC_GPIO_INPUT_HYSTERESIS_STANDARD                            /* Pad hysteresis of pin */
-};
-#endif
-#if (UC_SERIES == XMC47)
-const XMC_GPIO_CONFIG_t  ccu8_pwm_gpio1_config    =
-{
-   .mode                = XMC_GPIO_MODE_OUTPUT_PUSH_PULL_ALT3,                          /* Alternate Mode 3 to connect to CCU8 output */
-   .output_level        = XMC_GPIO_OUTPUT_LEVEL_LOW,                                    /* Output level of pin */
-   .output_strength     = XMC_GPIO_OUTPUT_STRENGTH_MEDIUM                               /* Pad driver mode */
-};
-const XMC_GPIO_CONFIG_t  ccu8_pwm_gpio2_config    =
-{
-   .mode                = XMC_GPIO_MODE_OUTPUT_PUSH_PULL_ALT3,                          /* Alternate Mode 3 to connect to CCU8 output */
-   .output_level        = XMC_GPIO_OUTPUT_LEVEL_LOW,                                    /* Output level of pin */
-   .output_strength     = XMC_GPIO_OUTPUT_STRENGTH_MEDIUM                               /* Pad driver mode */
-};
-#endif
-
-/*
-* Dead Time insertion configuration
-*/
-const XMC_CCU8_SLICE_DEAD_TIME_CONFIG_t  dt_config =
-    {
-      .enable_dead_time_channel1         = 1U,                                          /* Dead time insertion enabled for Ch1 */
-      .enable_dead_time_channel2         = 0U,                                          /* Dead time insertion disabled for Ch2 */
-      .channel1_st_path                  = 1U,                                          /* Dead time inserted for direct output of Ch1 */
-      .channel1_inv_st_path              = 1U,                                          /* Dead time inserted for invert output of Ch1 */
-      .channel2_st_path                  = 0U,                                          /* Dead time inserted for direct output of Ch2 */
-      .channel2_inv_st_path              = 0U,                                          /* Dead time inserted for invert output of Ch2 */
-      .div                               = (uint32_t)XMC_CCU8_SLICE_DTC_DIV_1,          /* Dead time pre-scaler divider value*/
-
-      .channel1_st_rising_edge_counter   = 22U,                                         /* Dead time inserted to rising edge of compare Ch1 */
-      .channel1_st_falling_edge_counter  = 22U,                                         /* Dead time inserted to falling edge of compare Ch1 */
-
-      .channel2_st_rising_edge_counter   = 0U,                                          /* Dead time inserted to rising edge of compare Ch2 */
-      .channel2_st_falling_edge_counter  = 0U,                                          /* Dead time inserted to falling edge of compare Ch2 */
-    };
 
 /*******************************************************************************
 * Function Name: main
@@ -226,40 +95,6 @@ int main(void)
     #if ENABLE_XMC_DEBUG_PRINT
     printf("Initialization done\r\n");
     #endif
-
-    /* Ensure fCCU reaches CCU8 module */
-    XMC_CCU8_SetModuleClock(CCU8_MODULE_PTR, XMC_CCU8_CLOCK_SCU);
-
-    /* Initialize CCU8 module */
-    XMC_CCU8_Init(CCU8_MODULE_PTR, XMC_CCU8_SLICE_MCMS_ACTION_TRANSFER_PR_CR);
-
-    /* Start the prescaler and restore clocks to slices */
-    XMC_CCU8_StartPrescaler(CCU8_MODULE_PTR);
-
-    /* Get the slices out of idle mode */
-    XMC_CCU8_EnableClock(CCU8_MODULE_PTR, PWM_SLICE_NUMBER);
-
-    /* Initialize the Slices */
-    XMC_CCU8_SLICE_CompareInit(PWM_SLICE_PTR, &ccu80_slice_pwm_config);
-
-    /* Program timer period for both ccu8 slices */
-    XMC_CCU8_SLICE_SetTimerPeriodMatch(PWM_SLICE_PTR, PWM_PERIOD);
-
-    /* Program initial compare match value for ccu8 slice */
-    XMC_CCU8_SLICE_SetTimerCompareMatch(PWM_SLICE_PTR, COMPARE_CHANNEL, PWM_COMPARE);
-
-    /* Configure GPIO outputs for the ccu8 slice */
-    XMC_GPIO_Init(PWM_OUTPUT_GPIO_PORT, PWM_OUTPUT_GPIO_PIN, &ccu8_pwm_gpio1_config);
-    XMC_GPIO_Init(PWM_INV_OUTPUT_GPIO_PORT, PWM_INV_OUTPUT_GPIO_PIN, &ccu8_pwm_gpio2_config);
-
-    /* Enable shadow transfer */
-    XMC_CCU8_EnableShadowTransfer(CCU8_MODULE_PTR, PWM_SHADOW_TRANSFER_MASK);
-
-    /* Configure Dead time insertion between transitions */
-    XMC_CCU8_SLICE_DeadTimeInit(PWM_SLICE_PTR, &dt_config);
-
-    /* Start timer */
-    XMC_CCU8_SLICE_StartTimer(PWM_SLICE_PTR);
 
     for (;;)
     {
